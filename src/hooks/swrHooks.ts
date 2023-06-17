00f7091b67f11392d06754218603fc51ff66b5ba
KyleJonesNV <kyle.jones@northvolt.com>
2023-06-17 09:56:25 +0200

    move swr hooks to hooks directory

---
+++ src/hooks/swrHooks.ts
@@ -0,0 +1,26 @@
+import { getAllForUser, getAllNotes } from '@/queries/queries'
+import { Topic } from '@/types/types'
+import useSWR from 'swr'
+
+export const userId = '1d7ee7f0-36f5-4e33-a766-26981e62d9cf'
+
+export function useUserTopics() {
+  const query = { id: userId }
+  const { data, error, isLoading, mutate: mutateTopic } = useSWR(query, getAllForUser)
+
+  const topics = data?.body as Topic[]
+
+  return { topics, error, isLoading, mutateTopic }
+}
+
+export function useNotesForTopic(topic: Topic | null) {
+  const query = {
+    UserID: userId,
+    Title: topic?.Title,
+  }
+  const { data: notesData, error, isLoading, mutate: mutateNotes } = useSWR(query, getAllNotes)
+
+  const notesForTopic = notesData?.body as Topic
+
+  return { notesForTopic, error, isLoading, mutateNotes }
+}
