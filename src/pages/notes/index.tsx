8d239e9cbf999de9434865aab9d3e57b3d9e63fd
KyleJonesNV <kyle.jones@northvolt.com>
2023-08-06 15:09:56 +0200

    add /notes

---
+++ src/pages/notes/index.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header/Header'
+import Content from '@/components/Content/Content'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <main>
+      <Head>
+        <title>Notes</title>
+      </Head>
+      <Header />
+      <Content />
+    </main>
+  )
+}
